dc2f18812c20b5fa097f99c7145c7aab73cb89b2
Jacob Taylor <jacobtaylor312@gmail.com>
2020-03-09 13:49:45 -0600

    Reorganized files and started project [master]

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header";
+import logo from "../logo.svg";
+import "../style/App.css";
+
+function App() {
+  return <Header></Header>;
+}
+
+export default App;

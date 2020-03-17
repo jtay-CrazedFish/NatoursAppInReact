710b00b1b932997661c753ec084313aa98095b1a
Jacob Taylor <jacobtaylor312@gmail.com>
2020-03-17 11:26:36 -0600

    Finished Features section.

--- src/components/FeatureBox.js
+++ src/components/FeatureBox.js
@@ -1,11 +1,12 @@
 import React from "react";
+import classnames from "classnames";
 
 function FeatureBox(props) {
   return (
     <div class="feature-box">
-      <i class="icon-basic-world"></i>
-      <h3 class="heading-tertiary">Explore the world</h3>
-      <p class="feature-box__text">Run around the world forever and ever,</p>
+      <i className={classnames("feature-box__icon", props.icon)}></i>
+      <h3 class="heading-tertiary u-margin-bottom-small">{props.header}</h3>
+      <p class="feature-box__text">{props.text}</p>
     </div>
   );
 }
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,13 +1,56 @@
 import React from "react";
-import classnames from "classnames";
 import FeatureBox from "./FeatureBox";
+
+const icons = {
+  WORLD: "icon-basic-world",
+  COMPASS: "icon-basic-compass",
+  MAP: "icon-basic-map",
+  HEART: "icon-basic-heart"
+};
+
+const headers = {
+  WORLD: "Explore the World",
+  COMPASS: "Meet Nature",
+  MAP: "Find your Way",
+  HEART: "live a healthier life"
+};
+
+const text = {
+  WORLD:
+    "Travel, travel, travel. That's what makes the world go round and round.",
+  COMPASS:
+    "Smell the trees and enjoy a nice time out with loved ones, and get away from the bustle of the city.",
+  MAP: "We have the map! Dora can now find where she is supposed to go!",
+  HEART: "Everything comes from the heart."
+};
 
 function Features(props) {
   return (
     <section class="section-features">
       <div class="row">
-        <div clas="col-1-of-4">
-          <FeatureBox />
+        <div class="col-1-of-4">
+          <FeatureBox
+            icon={icons.WORLD}
+            header={headers.WORLD}
+            text={text.WORLD}
+          />
+        </div>
+        <div class="col-1-of-4">
+          <FeatureBox
+            icon={icons.COMPASS}
+            header={headers.COMPASS}
+            text={text.COMPASS}
+          />
+        </div>
+        <div class="col-1-of-4">
+          <FeatureBox icon={icons.MAP} header={headers.MAP} text={text.MAP} />
+        </div>
+        <div class="col-1-of-4">
+          <FeatureBox
+            icon={icons.HEART}
+            header={headers.HEART}
+            text={text.HEART}
+          />
         </div>
       </div>
     </section>
